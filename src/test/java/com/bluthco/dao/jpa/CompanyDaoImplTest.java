42938907d7fafd9150b23d744c1081db54598f99
slowery <slowery23@gmail.com>
2014-07-01 17:52:19 -0500

    junits

--- src/test/java/com/bluthco/dao/jpa/CompanyDaoImplTest.java
+++ src/test/java/com/bluthco/dao/jpa/CompanyDaoImplTest.java
@@ -1,27 +1,86 @@
 package com.bluthco.dao.jpa;
 
-import static com.bluthco.model.Companies.BANANA_STAND;
-import static com.bluthco.model.Companies.BLUTH_COMPANY;
+import static com.bluthco.biz.BluthService.BANANA_STAND_NAME;
+import static com.bluthco.biz.BluthService.BLUTH_COMPANY_NAME;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-import org.junit.Assert;
+import org.jukito.JukitoRunner;
+import org.jukito.UseModules;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import com.bluthco.model.Companies;
 import com.bluthco.model.Company;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.persist.PersistService;
 import com.google.inject.persist.Transactional;
+import com.google.inject.persist.jpa.JpaPersistModule;
 
-public class CompanyDaoImplTest extends DaoImplTestBase {
+@RunWith(JukitoRunner.class)
+@UseModules(CompanyDaoImplTest.class)
+public class CompanyDaoImplTest extends AbstractModule {
+
+	@Inject
+	private static PersistService persistService;
+
+	@BeforeClass
+	public static void setUpClass() {
+		persistService.start();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		persistService.stop();
+	}
+
+	@Override
+	protected void configure() {
+		requestStaticInjection(CompanyDaoImplTest.class);
+		install(new JpaPersistModule("example"));
+	}
+
+	@Singleton
+	@Provides
+	@Named(BLUTH_COMPANY_NAME)
+	protected Company provideBluthCompany() {
+		return Company.from(BLUTH_COMPANY_NAME);
+	}
+
+	@Singleton
+	@Provides
+	@Named(BANANA_STAND_NAME)
+	protected Company provideBananaStand() {
+		return Company.from(BANANA_STAND_NAME);
+	}
 
 	@Before
 	@Transactional
-	public void setUp(CompanyDaoImpl companyDao) {
-		companyDao.persist(BLUTH_COMPANY, BANANA_STAND);
+	public void setUp(CompanyDaoImpl companyDao, @Named(BLUTH_COMPANY_NAME) Company bluthCompany, @Named(BANANA_STAND_NAME) Company bananaStand) {
+		companyDao.persist(bluthCompany, bananaStand);
 	}
 
 	@Test
-	public void test(CompanyDaoImpl companyDao) {
-		Company queried = companyDao.findByName(Companies.BLUTH_COMPANY.getName());
-		Assert.assertEquals(Companies.BLUTH_COMPANY, queried);
+	public void testBluthCompany(CompanyDaoImpl companyDao, @Named(BLUTH_COMPANY_NAME) Company bluthCompany) {
+		Company queried = companyDao.findByName(BLUTH_COMPANY_NAME);
+		assertEquals(bluthCompany, queried);
+	}
+
+	@Test
+	public void testBananaStand(CompanyDaoImpl companyDao, @Named(BANANA_STAND_NAME) Company bananaStand) {
+		Company queried = companyDao.findByName(BANANA_STAND_NAME);
+		assertEquals(bananaStand, queried);
+	}
+
+	@Test
+	public void testUnknown(CompanyDaoImpl companyDao) {
+		Company queried = companyDao.findByName("something else");
+		assertNull(queried);
 	}
 }
--- src/test/java/com/bluthco/dao/jpa/EmployeeDaoImplTest.java
+++ src/test/java/com/bluthco/dao/jpa/EmployeeDaoImplTest.java
@@ -1,39 +1,87 @@
 package com.bluthco.dao.jpa;
 
-import static com.bluthco.model.Companies.BANANA_STAND;
-import static com.bluthco.model.Companies.BLUTH_COMPANY;
-import static com.bluthco.model.Employees.BUSTER;
-import static com.bluthco.model.Employees.GEORGE;
-import static com.bluthco.model.Employees.GEORGE_MICHAEL;
-import static com.bluthco.model.Employees.GOB;
-import static com.bluthco.model.Employees.LINDSEY;
-import static com.bluthco.model.Employees.LUCILLE;
-import static com.bluthco.model.Employees.MAEBY;
-import static com.bluthco.model.Employees.MICHAEL;
+import static com.bluthco.biz.BluthService.BANANA_STAND_NAME;
+import static com.bluthco.biz.BluthService.BLUTH_COMPANY_NAME;
 
+import org.jukito.JukitoRunner;
+import org.jukito.UseModules;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import com.bluthco.model.Company;
 import com.bluthco.model.Employee;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.persist.PersistService;
 import com.google.inject.persist.Transactional;
+import com.google.inject.persist.jpa.JpaPersistModule;
 
-public class EmployeeDaoImplTest extends DaoImplTestBase {
+@RunWith(JukitoRunner.class)
+@UseModules(EmployeeDaoImplTest.class)
+public class EmployeeDaoImplTest extends AbstractModule {
+
+	@Inject
+	private static PersistService persistService;
+
+	@BeforeClass
+	public static void setUpClass() {
+		persistService.start();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		persistService.stop();
+	}
+
+	@Override
+	protected void configure() {
+		requestStaticInjection(EmployeeDaoImplTest.class);
+		install(new JpaPersistModule("example"));
+	}
+
+	@Singleton
+	@Provides
+	@Named(BLUTH_COMPANY_NAME)
+	protected Company provideBluthCompany() {
+		return Company.from(BLUTH_COMPANY_NAME);
+	}
+
+	@Singleton
+	@Provides
+	@Named(BANANA_STAND_NAME)
+	protected Company provideBananaStand() {
+		return Company.from(BANANA_STAND_NAME);
+	}
 
 	@Before
 	@Transactional
-	public void setUp(CompanyDaoImpl companyDao, EmployeeDaoImpl employeeDao) {
-		companyDao.persist(BLUTH_COMPANY, BANANA_STAND);
+	public void setUp(EmployeeDaoImpl employeeDao, @Named(BLUTH_COMPANY_NAME) Company bluthCompany, @Named(BANANA_STAND_NAME) Company bananaStand) {
+		// companyDao.persist(bluthCompany, bananaStand);
+
 		employeeDao.persist(new Employee[] {
-				GEORGE, LUCILLE, MICHAEL, GOB, LINDSEY, BUSTER, GEORGE_MICHAEL, MAEBY
+				Employee.from(bluthCompany, "Bluth", "George"),
+				Employee.from(bluthCompany, "Bluth", "Lucille"),
+				Employee.from(bluthCompany, "Bluth", "Michael"),
+				Employee.from(bluthCompany, "Bluth", "Gob"),
+				Employee.from(bluthCompany, "Bluth", "Lindsey"),
+				Employee.from(bluthCompany, "Bluth", "Buster"),
+				Employee.from(bananaStand, "Bluth", "George Michael"),
+				Employee.from(bananaStand, "Bluth", "Maeby")
 		});
 	}
 
 	@Test
-	public void test(EmployeeDaoImpl employeeDao) {
-		Assert.assertEquals(6, employeeDao.findByCompany(BLUTH_COMPANY, 0, 10).size());
-		Assert.assertEquals(2, employeeDao.findByCompany(BANANA_STAND, 0, 10).size());
-		Assert.assertEquals(2, employeeDao.findByCompany(BLUTH_COMPANY, 0, 2).size());
-		Assert.assertEquals(1, employeeDao.findByCompany(BLUTH_COMPANY, 3, 1).size());
+	public void testQueries(EmployeeDaoImpl employeeDao, @Named(BLUTH_COMPANY_NAME) Company bluthCompany, @Named(BANANA_STAND_NAME) Company bananaStand) {
+		Assert.assertEquals(6, employeeDao.findByCompany(bluthCompany, 0, 10).size());
+		Assert.assertEquals(2, employeeDao.findByCompany(bananaStand, 0, 10).size());
+		Assert.assertEquals(2, employeeDao.findByCompany(bluthCompany, 0, 2).size());
+		Assert.assertEquals(1, employeeDao.findByCompany(bluthCompany, 3, 1).size());
 	}
 }
