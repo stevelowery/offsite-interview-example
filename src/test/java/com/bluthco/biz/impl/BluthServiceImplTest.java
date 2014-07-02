c5e309ca7d797dcaddc7a28bec6ed7897338a939
slowery <slowery23@gmail.com>
2014-07-02 10:19:22 -0500

    BluthServiceImplTest implementation

--- src/test/java/com/bluthco/biz/impl/BluthServiceImplTest.java
+++ src/test/java/com/bluthco/biz/impl/BluthServiceImplTest.java
@@ -3,109 +3,81 @@ package com.bluthco.biz.impl;
 import static com.bluthco.biz.BluthService.BANANA_STAND_NAME;
 import static com.bluthco.biz.BluthService.BLUTH_COMPANY_NAME;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
-import org.jukito.JukitoRunner;
-import org.jukito.UseModules;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import com.bluthco.biz.BluthService;
 import com.bluthco.dao.CompanyDao;
 import com.bluthco.dao.EmployeeDao;
-import com.bluthco.dao.jpa.CompanyDaoImpl;
-import com.bluthco.dao.jpa.EmployeeDaoImpl;
 import com.bluthco.model.Company;
 import com.bluthco.model.Employee;
-import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import com.google.inject.persist.PersistService;
-import com.google.inject.persist.Transactional;
-import com.google.inject.persist.jpa.JpaPersistModule;
+import com.google.inject.util.Providers;
 
-@RunWith(JukitoRunner.class)
-@UseModules(BluthServiceImplTest.class)
-public class BluthServiceImplTest extends AbstractModule {
+@RunWith(MockitoJUnitRunner.class)
+public class BluthServiceImplTest {
 
-	@Inject
-	private static PersistService persistService;
+	@Mock
+	private CompanyDao companyDao;
+	@Mock
+	private EmployeeDao employeeDao;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		persistService.start();
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		persistService.stop();
-	}
+	private BluthService bluthService;
 
 	@Before
-	@Transactional
-	public void setUp(CompanyDao companyDao, EmployeeDao employeeDao) {
-
-		Company bluthCompany = Company.from(BLUTH_COMPANY_NAME);
-		Company bananaStand = Company.from(BANANA_STAND_NAME);
-		companyDao.persist(bluthCompany, bananaStand);
-
-		Employee[] employees = new Employee[] {
-				Employee.from(bluthCompany, "Michael", "Bluth"),
-				Employee.from(bluthCompany, "Gob", "Bluth"),
-				Employee.from(bluthCompany, "Lindsey", "Bluth"),
-				Employee.from(bluthCompany, "Buster", "Bluth"),
-				Employee.from(bluthCompany, "Lucille", "Bluth"),
-				Employee.from(bluthCompany, "George Sr", "Bluth"),
-				Employee.from(bananaStand, "George Michael", "Bluth"),
-				Employee.from(bananaStand, "Maeby", "Funke")
-		};
-		employeeDao.persist(employees);
+	public void setUp() {
+		bluthService = new BluthServiceImpl(Providers.of(companyDao), Providers.of(employeeDao));
 	}
 
 	@Test
-	public void testBananaStand(BluthServiceImpl bluthService) {
+	public void testBananaStand() {
+		Company company = Company.from(BANANA_STAND_NAME);
+		when(companyDao.findByName(BANANA_STAND_NAME)).thenReturn(company);
+
 		Company bananaStand = bluthService.findBananaStand();
-		assertNotNull(bananaStand);
-		assertEquals(BANANA_STAND_NAME, bananaStand.getName());
+		assertEquals(bananaStand, company);
+
+		verify(companyDao, only()).findByName(BANANA_STAND_NAME);
 	}
 
 	@Test
-	public void testBluthCompany(BluthServiceImpl bluthService) {
-		Company bluthCompany = bluthService.findBluthCompany();
-		assertNotNull(bluthCompany);
-		assertEquals(BLUTH_COMPANY_NAME, bluthCompany.getName());
+	public void testBluthCompany() {
+		Company company = Company.from(BLUTH_COMPANY_NAME);
+		when(companyDao.findByName(BLUTH_COMPANY_NAME)).thenReturn(company);
+
+		Company bananaStand = bluthService.findBluthCompany();
+		assertEquals(bananaStand, company);
+
+		verify(companyDao, only()).findByName(BLUTH_COMPANY_NAME);
 	}
 
 	@Test
-	public void testBananaStandEmployees(BluthServiceImpl bluthService) {
+	public void testBananaStandEmployees() {
+		Company bananaStand = Company.from(BANANA_STAND_NAME);
+
+		List<Employee> expected = Arrays.asList(new Employee[] {
+				Employee.from(bananaStand, "Bluth", "George Michael"), Employee.from(bananaStand, "Funke", "Maeby")
+		});
+		when(companyDao.findByName(BANANA_STAND_NAME)).thenReturn(bananaStand);
+		when(employeeDao.findByCompany(bananaStand, 0, 10)).thenReturn(expected);
+
 		List<Employee> employees = bluthService.findBananaStandEmployees();
-		assertNotNull(employees);
 		assertEquals(2, employees.size());
+		assertEquals(expected.get(0), employees.get(0));
+		assertEquals(expected.get(1), employees.get(1));
 	}
 
 	@Test(expected = UnsupportedOperationException.class)
-	public void testFindAll(BluthServiceImpl bluthService) {
+	public void testFindAll() {
 		bluthService.findAllEmployees();
-	}
-
-	@After
-	@Transactional
-	public void tearDown(EntityManager entityManager) {
-		entityManager.createNativeQuery("truncate table employee").executeUpdate();
-		entityManager.createNativeQuery("truncate table company").executeUpdate();
-	}
-
-	@Override
-	protected void configure() {
-		requestStaticInjection(BluthServiceImplTest.class);
-		bind(CompanyDao.class).to(CompanyDaoImpl.class);
-		bind(EmployeeDao.class).to(EmployeeDaoImpl.class);
-		install(new JpaPersistModule("example"));
 	}
 }
